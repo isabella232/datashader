f9cab329483d595e4db9901c77bcdb7808d9caac
Jon Mease <jon.mease@gmail.com>
2019-10-22 11:11:05 -0400

    Test polygon with aggregate

--- datashader/tests/test_polygons.py
+++ datashader/tests/test_polygons.py
@@ -2,7 +2,7 @@ import pytest
 import pandas as pd
 import numpy as np
 import xarray as xr
-from math import inf
+from math import inf, nan
 import datashader as ds
 from datashader.tests.test_pandas import assert_eq_xr
 import dask.dataframe as dd
@@ -149,4 +149,50 @@ def test_no_overlap(DataFrame):
 
     out = xr.DataArray(sol, coords=[lincoords_y, lincoords_x], dims=['y', 'x'])
 
+    assert_eq_xr(agg, out)
+
+
+@pytest.mark.parametrize('DataFrame', DataFrames)
+def test_no_overlap_agg(DataFrame):
+    df = DataFrame({
+        'polygons': pd.Series([
+            [1, 1, 2, 2, 1, 3, 0, 2, 1, 1,
+             -inf, -inf,
+             0.5, 1.5, 0.5, 2.5, 1.5, 2.5, 1.5, 1.5, 0.5, 1.5],
+            [0.5, 1.5, 1.5, 1.5, 1.5, 2.5, 0.5, 2.5, 0.5, 1.5],
+            [0, 1, 2, 1, 2, 3, 0, 3, 0, 1,
+             1, 1, 0, 2, 1, 3, 2, 2, 1, 1]
+        ], dtype='Polygons[float64]'),
+        'v': range(3)
+    })
+
+    cvs = ds.Canvas(plot_width=16, plot_height=16)
+    agg = cvs.polygons(df, geometry='polygons', agg=ds.sum('v'))
+
+    axis = ds.core.LinearAxis()
+    lincoords_x = axis.compute_index(
+        axis.compute_scale_and_translate((0, 2), 16), 16)
+    lincoords_y = axis.compute_index(
+        axis.compute_scale_and_translate((1, 3), 16), 16)
+
+    sol = np.array([
+        [nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan],
+        [nan,  2.,  2.,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  2.,  2.,  2., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  0.,  0.,  2.,  2., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  0.,  0.,  0.,  0.,  2., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  2.,  2.],
+        [nan,  2.,  2.,  0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  0.,  2.],
+        [nan,  2.,  0.,  0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  0.,  0.],
+        [nan,  0.,  0.,  0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  0.,  0.],
+        [nan,  2.,  0.,  0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  0.,  0.],
+        [nan,  2.,  2.,  0.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  0.,  2.],
+        [nan,  2.,  2.,  2.,  0.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 0.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  1.,  1.,  1.,  1.,  1.,  1.,  1.,  1., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  0.,  0.,  0.,  0.,  2., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  0.,  0.,  2.,  2., 2.,  2.,  2.],
+        [nan,  2.,  2.,  2.,  2.,  2.,  2.,  2.,  0.,  0.,  2.,  2.,  2., 2.,  2.,  2.]
+    ])
+
+    out = xr.DataArray(sol, coords=[lincoords_y, lincoords_x], dims=['y', 'x'])
     assert_eq_xr(agg, out)
